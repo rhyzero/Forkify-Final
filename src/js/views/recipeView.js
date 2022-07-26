a07bafc584a2defbb4a5cee27ca0956f7defc340
rhyzero <jackyc4200@gmail.com>
2022-07-26 00:20:30 -0400

    fixed fractional library error

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg'
-import { Fraction } from 'fractional';
+import { numberToFraction } from '../helpers.js';
 import View from './view'
 import * as model from '../model.js'
 
@@ -114,7 +114,7 @@ class recipeView extends View{
             <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}</div>
+            <div class="recipe__quantity">${ingredient.quantity ? numberToFraction(ingredient.quantity).toString() : ''}</div>
             <div class="recipe__description">
             <span class="recipe__unit">${ingredient.unit}</span>
             ${ingredient.description}
